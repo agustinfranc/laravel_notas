b83f198d405a0a19574b56addefec62880853edf
Agustin <agustin.amato96@gmail.com>
2019-07-10 23:01:06 -0300

    rutas modoficado

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,6 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::resource('/notas', 'NotaController');
+
+Route::get('/redirect/{provider}', 'Auth\LoginController@redirectToProvider');
+Route::get('/callback/{provider}', 'Auth\LoginController@handleProviderCallback');
