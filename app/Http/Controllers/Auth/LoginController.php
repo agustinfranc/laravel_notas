1dde6a1018d7caa93b723bfc2f4bacde603a25b4
Agustin <agustin.amato96@gmail.com>
2019-07-10 23:03:49 -0300

    dirigo a notas

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -66,7 +66,7 @@ class LoginController extends Controller
 
             Auth::login($findUser);
 
-            return view('home');
+            return view('notas');
         }
         else {
 
@@ -78,7 +78,7 @@ class LoginController extends Controller
 
             Auth::login($user);
 
-            return view('home');
+            return view('notas');
         }
 
         //return ($user->getId());
